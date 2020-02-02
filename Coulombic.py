1bc30c3b0b2f4fa5e54bad179372115d0e40d9e3
Benjamin Björnsson <benjaminbjornsson@Benjamins-MacBook-Pro.local>
2020-02-02 16:18:48 +0100

    Separated classes previosly in Potential.py into separate files

---
+++ Coulombic.py
@@ -0,0 +1,12 @@
+class Coulombic(CoulombCentrifugalBarrier):
+	def __init__(self, l, V_0, R):
+		CoulombCentrifugalBarrier.__init__(self, l, V_0, R)
+
+	def __call__(self, r):
+		l = self.getAngularQuantumNum()
+		R = self.getShellRadius()
+		if r > R:
+			V = 1/r + l*(l+1)/(2*r**2)
+		else:
+			V = self.getTrapPotential()
+		return V
---
+++ Dielectric.py
@@ -0,0 +1,26 @@
+class Dielectric(CoulombCentrifugalBarrier):
+	def __init__(self, l, V_0, R, dielectricConstant):
+		CoulombCentrifugalBarrier.__init__(self, l, V_0, R)
+		self.dielectricConstant = dielectricConstant
+		self.delta = 0.0
+		self.delta = self.bisectionSearch(self.getTrapPotential(), [R, R + 2.0], 1, 1e-7) - R
+
+	def getDielectricConstant(self):
+		return self.dielectricConstant
+
+	def getDelta(self):
+		return self.delta
+
+	def __call__(self, r):
+		l = self.getAngularQuantumNum()
+		R = self.getShellRadius()
+		delta = self.getDelta()
+		dielectricConstant = self.getDielectricConstant()
+		if r > R + delta:
+			V = -((dielectricConstant - 1)*R**3)/(2*(dielectricConstant + 2)*r**2*(r**2 - R**2)) + 1/r + l*(l+1)/(2*r**2)
+		else:
+			V = self.getTrapPotential()
+		return V
+
+	def getIntersect(self, Energy, searchRange, slopeSign, epsilon):
+		return self.bisectionSearch(Energy, searchRange, slopeSign, epsilon)
