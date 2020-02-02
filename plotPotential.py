1d4f6822a891b31b23d6739c68438044aa13290d
Benjamin Björnsson <benjaminbjornsson@Benjamins-MacBook-Pro.local>
2020-02-02 10:46:34 +0100

    Updating plotPotential: removing argument delta passed to class Dielectric

--- plotPotential.py
+++ plotPotential.py
@@ -7,7 +7,7 @@ beta = 27.211
 
 r = numpy.linspace(0.0, 75.0/alpha, 1e6)
 
-V = Dielectric(0, 0.5/beta, 5.01/alpha, 0.4/alpha, 4.4)
+V = Dielectric(0, 0.5/beta, 5.01/alpha, 4.4)
 
 pylab.figure(1)
 
@@ -21,5 +21,3 @@ pylab.plot(r, V_data_plot)
 pylab.xlabel('r [Å]')
 pylab.ylabel('V(r) [eV]')
 pylab.savefig('testPlot')
-
-print(V_data_plot[-1])
