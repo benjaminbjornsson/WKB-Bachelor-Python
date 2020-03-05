2fd1d33c7d8c0e146e1417438d9214d1d6c2cb1b
Benjamin Björnsson <benjaminbjornsson@Benjamins-MBP.localdomain>
2020-03-05 12:59:59 +0100

    added heat_capacity with functions for internal energy and heat capacity

---
+++ heat_capacity.py
@@ -0,0 +1,46 @@
+import numpy
+import matplotlib.pyplot
+
+Hartree2eV = 27.211386245988    # Conversion factor for Hatree -> eV
+eV2Hartree = 1/Hartree2eV       #    - || -  - || -     eV -> Hartree
+
+cm2eV = (1.23984193*1e-4)
+k_B = 8.617333262145e-5         # eV/K
+nu = numpy.array([610, 1667, 
+                627, 865, 1410, 
+                591, 784, 919, 1483, 
+                491, 579, 856, 1235, 1404, 1650, 
+                263, 447, 771, 924, 1261, 1407, 1596, 1722,
+                972, 
+                577, 719, 1353, 1628, 
+                348, 776, 1134, 1314, 1687, 
+                362, 750, 914, 1110, 1436, 1587, 
+                403, 546, 706, 822, 1344, 1467, 1709])    # cm-1
+
+nu = numpy.where(nu > 1000, 0.9*nu, nu)
+
+degeneracy = numpy.array([1, 2,
+                1, 2, 3,
+                1*2, 2*2, 2*3, 2*4,
+                1, 2, 3, 4, 5, 6,
+                1, 2, 3, 4, 5, 6, 7, 8,
+                1,
+                1, 2, 3, 4,
+                1*1, 2*2, 2*3, 2*4, 2*5,
+                1, 2, 3, 4, 5, 6,
+                1, 2, 3, 4, 5, 6, 7])
+
+energy = cm2eV*nu # eV
+
+def expected_energy(temperature):
+    exponential_factors = numpy.exp(energy/(k_B*temperature))
+    return numpy.sum(numpy.multiply(degeneracy, numpy.divide(energy, exponential_factors - 1.0)))
+
+def heat_capacity(temperature):
+    exponential_factors = numpy.exp(energy/(k_B*temperature))
+    return 1/(k_B*temperature**2)*numpy.sum(numpy.multiply(numpy.multiply(degeneracy, exponential_factors), numpy.divide(numpy.square(energy), numpy.square(exponential_factors - 1.0))))
+
+expected_energy_vectorized = numpy.vectorize(expected_energy)
+heat_capacity_vectorized = numpy.vectorize(heat_capacity)
+
+linear_energy = lambda T: 7.4 + 0.0138*(T - 1000)
