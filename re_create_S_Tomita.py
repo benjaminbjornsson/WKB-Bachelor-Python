d3a1ce949045b2845f508686490561f4793b9166
Benjamin Björnsson <benjaminbjornsson@Benjamins-MBP.localdomain>
2020-03-05 13:02:50 +0100

    Added plotting of heat capacity

--- re_create_S_Tomita.py
+++ re_create_S_Tomita.py
@@ -1,18 +1,22 @@
 import numpy
 import matplotlib.pyplot
 import scipy.integrate
+import scipy.special
 
-from Metallic_Tomita import *
+from heat_capacity import *
+
+from Metallic import *
 
 def WKB_Integral(V, E, Range, epsilon):
     def kappa(r, V, Energy):
         return numpy.sqrt(2*(V(r) - Energy))
 
-    return scipy.integrate.quad(kappa, Range[0], Range[1], args=(V, Energy), full_output = 0, epsabs = epsilon)[0]
+    return 2*scipy.integrate.quad(kappa, Range[0], Range[1], args=(V, Energy), full_output = 0, epsabs = epsilon)[0]
 
-Bohr2Angstrom = 0.529177210     # Conversion factor for Bohr -> Ångström
-Angstrom2Bohr = 1/Bohr2Angstrom #    - || -  - || -     Ångström -> Bohr
-Hartree2eV = 27.211386245       # Conversion factor for Hatree -> eV
+
+Bohr2Angstrom = 0.529           # Conversion factor for Bohr -> Angstrom
+Angstrom2Bohr = 1/Bohr2Angstrom #    - || -  - || -      Angstrom -> Bohr
+Hartree2eV = 27.211386245988    # Conversion factor for Hatree -> eV
 eV2Hartree = 1/Hartree2eV       #    - || -  - || -     eV -> Hartree
 
 angularQuantumNumber = 1
@@ -22,16 +26,68 @@ V_0 = 0.0               # Hartree
 WKB_epsilon = 1e-10             # Integration error WKB_Integral
 Bisection_epsilon = 1e-10       # Bisection search error
 
-# Create potential of class 'Metallic_Timita'
-V = Metallic_Tomita(angularQuantumNumber, V_0, Angstrom2Bohr*shellRadius)
+# Create potential of class 'Metallic'
+V = Metallic(angularQuantumNumber, V_0, Angstrom2Bohr*shellRadius)
 
 # Create r in range [0.0 80.0] Ångström
-r = numpy.linspace(0.0, 80.0, 1e5)
+r = numpy.linspace(0.0, 80.0, int(1e5))
 
 # Plot potential in eV vs Ångström(Fig.6 in article)
+#fig, ax = matplotlib.pyplot.subplots()
+#ax.plot(r, Hartree2eV*V(Angstrom2Bohr*r))
+#ax.set(xlabel='r (Å)', ylabel='U(r) (eV)', title='Fig. 6 Article S. Tomita')
+#matplotlib.pyplot.xlim(0.0, 90.0)
+#matplotlib.pyplot.ylim(0.0, 1.6)
+#matplotlib.pyplot.show()
+
+# Plot expected_energy and heat_capacity
+#temperature = numpy.linspace(10.0, 2000, int(1e4))
+#fig, (ax1, ax2) = matplotlib.pyplot.subplots(1,2)
+
+#ax1.plot(temperature, expected_energy_vectorized(temperature))
+#ax1.plot(temperature[temperature > 500], linear_energy(temperature[temperature > 500]))
+#ax2.plot(temperature, heat_capacity_vectorized(temperature))
+
+#matplotlib.pyplot.show()
+
+# Plot populations and rate(Fig 8. in article)
+k_B = 8.617333262145e-5                                 # eV/K
+epsilon = numpy.array([0.4, 0.2, 0.222, 0.305, 0.335])  # eV
+A_0 = 7.0e8                                             # s-1
+degeneracy_C60 = numpy.array([0.0, 1, 1, 3, 3])
+
+def probabilities(temperature, i):
+    denomenator = (k_B*(temperature - epsilon[i]/(2*heat_capacity(temperature))))
+    numerator = -epsilon[i]
+#    return degeneracy_C60[i]*numpy.exp(numerator/denomenator)
+    return denomenator
+    return numerator/denomenator
+    
+
+#    return degeneracy_C60[i]*numpy.exp(-(epsilon[i])/(k_B*(temperature - epsilon[i]/(2*heat_capacity(temperature)))))
+
+def k(temperature, i):
+    if i == 0:
+        return A_0*numpy.exp(-epsilon[i]/k_B*numpy.divide(1, temperature))
+
+temperature = numpy.linspace(100.0, 500.0, int(1e3))
+
+#fig, ax = matplotlib.pyplot.subplots()
+
+#ax.semilogy(temperature, k(temperature, 0))
+
+#matplotlib.pyplot.xlim(0, 500)
+#matplotlib.pyplot.ylim(1e-1, 1e5)
+
+#matplotlib.pyplot.show()
+
+probabilities_vectorized = numpy.vectorize(probabilities)
+
 fig, ax = matplotlib.pyplot.subplots()
-ax.plot(r, Hartree2eV*V(Angstrom2Bohr*r))
-ax.set(xlabel='r (Å)', ylabel='U(r) (eV)', title='Fig. 6 Article S. Tomita')
-matplotlib.pyplot.xlim(0.0, 90.0)
-matplotlib.pyplot.ylim(0.0, 1.6)
+
+ax.plot(temperature, probabilities_vectorized(temperature, 1))
+
+#matplotlib.pyplot.xlim(0, 500)
+#matplotlib.pyplot.ylim(0.0, 1.0)
+
 matplotlib.pyplot.show()
